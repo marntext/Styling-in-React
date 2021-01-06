ecd798580ac3be9b74ab4cc5f3eb10208da21e8d
marntext <marntext@gmail.com>
2021-01-06 08:13:35 +0300

    first commit

---
+++ components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./Card.module.css"
+
+
+
+export const Card = () => {
+    return (
+        <div className= {styles.cardWrapper}>
+            <p>CARD</p>
+        </div>
+    )
+}
+
